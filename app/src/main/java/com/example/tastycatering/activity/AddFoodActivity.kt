da127395ef31f8f3d5f012a1aa1e519594d4de0d
fayis672 <43964053+fayis672@users.noreply.github.com>
2020-05-30 22:00:05 +0530

    first test

---
+++ app/src/main/java/com/example/tastycatering/activity/AddFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tastycatering.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tastycatering.R
+
+class AddFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_food)
+    }
+}
---
+++ app/src/main/java/com/example/tastycatering/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tastycatering.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tastycatering.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
