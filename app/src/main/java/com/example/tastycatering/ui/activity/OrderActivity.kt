09d078224fec56842bc38c3661c0a7f3c7d686c1
fayis <fayistkm36@gmail.com>
2020-09-05 20:25:56 +0530

    qty ui added

--- app/src/main/java/com/example/tastycatering/ui/activity/OrderActivity.kt
+++ app/src/main/java/com/example/tastycatering/ui/activity/OrderActivity.kt
@@ -2,13 +2,24 @@ package com.example.tastycatering.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.tastycatering.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.app_bar.*
 
 @AndroidEntryPoint
 class OrderActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        setSupportActionBar(app_bar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(true)
     }
 }
